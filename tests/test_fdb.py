4df9e0af6c0130b709d5efd1333dee1566bcc655
Qi Luo <qiluo-msft@users.noreply.github.com>
2017-01-26 00:41:00 +0000

    Fix: GetPDU contructs search range, add test case for GetNextPDU

--- tests/test_fdb.py
+++ tests/test_fdb.py
@@ -11,7 +11,7 @@ import tests.mock_tables.dbconnector
 
 from ax_interface.mib import MIBTable
 from ax_interface.pdu import PDUHeader
-from ax_interface.pdu_implementations import GetPDU
+from ax_interface.pdu_implementations import GetPDU, GetNextPDU
 from ax_interface import ValueType
 from ax_interface.encodings import ObjectIdentifier
 from ax_interface.constants import PduTypes
@@ -44,4 +44,38 @@ class TestFdbMIB(TestCase):
         self.assertEqual(value0.type_, ValueType.INTEGER)
         self.assertEqual(value0.data, 49)
 
+    def test_getnextpdu(self):
+        get_pdu = GetNextPDU(
+            header=PDUHeader(1, PduTypes.GET, 16, 0, 42, 0, 0, 0),
+            oids=(
+                ObjectIdentifier(20, 0, 0, 0, (1, 3, 6, 1, 2, 1, 17, 7, 1, 2, 2, 1, 2)),
+            )
+        )
+
+        encoded = get_pdu.encode()
+        response = get_pdu.make_response(self.lut)
+        print(response)
+
+        n = len(response.values)
+        # self.assertEqual(n, 7)
+        value0 = response.values[0]
+        self.assertEqual(value0.type_, ValueType.INTEGER)
+        self.assertEqual(value0.data, 49)
+
+    def test_getnextpdu_empty(self):
+        get_pdu = GetNextPDU(
+            header=PDUHeader(1, PduTypes.GET, 16, 0, 42, 0, 0, 0),
+            oids=(
+                ObjectIdentifier(20, 0, 0, 0, (1, 3, 6, 1, 2, 1, 17, 7, 1, 2, 2, 1, 3)),
+            )
+        )
+
+        encoded = get_pdu.encode()
+        response = get_pdu.make_response(self.lut)
+        print(response)
+
+        n = len(response.values)
+        # self.assertEqual(n, 7)
+        value0 = response.values[0]
+        self.assertEqual(value0.type_, ValueType.END_OF_MIB_VIEW)
 
