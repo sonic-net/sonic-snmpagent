d0a64424ec30558725971bf0cc147c53974b42e4
SuvarnaMeenakshi <sumeenak@microsoft.com>
2020-05-28 16:06:52 -0700

    Add namespace test case for csqIfQosGroupStatsTable.
    Signed-off-by: SuvarnaMeenakshi <sumeenak@microsoft.com>

--- src/sonic_ax_impl/mibs/__init__.py
+++ src/sonic_ax_impl/mibs/__init__.py
@@ -475,11 +475,14 @@ class Namespace:
         """
         db get_all function executed on global and all namespace DBs.
         """
+        result = {}
         for db_conn in dbs:
             db_conn.connect(db_name)
             if(db_conn.exists(db_name, _hash)):
-                return db_conn.get_all(db_name, _hash, *args, **kwargs)
-        return {}
+                ns_result = db_conn.get_all(db_name, _hash, *args, **kwargs)
+                if ns_result is not None:
+                    result.update(ns_result)
+        return result
 
     @staticmethod
     def get_non_host_dbs(dbs):
