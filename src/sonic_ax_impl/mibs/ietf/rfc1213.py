4d5d90047309144e7ffa185b6ca9903f4ff39a24
Suvarna Meenakshi <sumeenak@microsoft.com>
2020-12-17 22:48:48 +0000

    [sysName]: Add sysName OID implementation in snmpagent. sysName OID is currently supported by snmpd. Override this implementation to make sure the latest hostname is taken from config db.
    Signed-off-by: Suvarna Meenakshi <sumeenak@microsoft.com>

--- src/sonic_ax_impl/mibs/ietf/rfc1213.py
+++ src/sonic_ax_impl/mibs/ietf/rfc1213.py
@@ -667,3 +667,33 @@ class InterfacesMIB(metaclass=MIBMeta, prefix='.1.3.6.1.2.1.2'):
     # FIXME Placeholder
     ifSpecific = \
         SubtreeMIBEntry('2.1.22', if_updater, ValueType.OBJECT_IDENTIFIER, lambda sub_id: ObjectIdentifier.null_oid())
+
+class sysNameUpdater(MIBUpdater):
+    def __init__(self):
+        super().__init__()
+        self.db_conn = mibs.init_db()
+
+    def update_data(self):
+        return
+
+    def get_sys_name(self):
+        """
+        Subclass update interface information
+        """
+        self.db_conn.connect(mibs.CONFIG_DB)
+
+        device_metadata = self.db_conn.get_all(self.db_conn.CONFIG_DB, "DEVICE_METADATA|localhost")
+
+        if device_metadata is not None and 'hostname' in device_metadata:
+             return str(device_metadata['hostname'])
+        else:
+             return None
+
+
+class SysNameMIB(metaclass=MIBMeta, prefix='.1.3.6.1.2.1.1.5'):
+    """
+
+    """
+    updater = sysNameUpdater()
+
+    sysName = MIBEntry('0', ValueType.OCTET_STRING, updater.get_sys_name)
