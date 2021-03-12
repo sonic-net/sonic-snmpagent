9749374022771138fde9651348ba84d89e77dfb8
Abhishek Dosi <abdosi@microsoft.com>
2021-03-12 21:51:45 +0000

    Updated for Multi-asic
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>

--- tests/namespace/test_lldp.py
+++ tests/namespace/test_lldp.py
@@ -124,10 +124,35 @@ class TestLLDPMIB(TestCase):
 
 
     def test_subtype_lldp_rem_man_addr_table(self):
+        # Verfiy both valid ipv4 and ipv6 address exit
         for entry in range(3, 6):
             mib_entry = self.lut[(1, 0, 8802, 1, 1, 2, 1, 4, 2, 1, entry)]
             ret = mib_entry(sub_id=(1, 1))
             self.assertIsNotNone(ret)
+            print(ret)
+        # Verfiy both valid ipv4 and invalid ipv6 address exit
+        for entry in range(3, 6):
+            mib_entry = self.lut[(1, 0, 8802, 1, 1, 2, 1, 4, 2, 1, entry)]
+            ret = mib_entry(sub_id=(1, 5))
+            self.assertIsNotNone(ret)
+            print(ret)
+        # Verfiy only valid ipv4 address exit
+        for entry in range(3, 6):
+            mib_entry = self.lut[(1, 0, 8802, 1, 1, 2, 1, 4, 2, 1, entry)]
+            ret = mib_entry(sub_id=(1, 9))
+            self.assertIsNotNone(ret)
+            print(ret)
+        # Verfiy only valid ipv6 address exit
+        for entry in range(3, 6):
+            mib_entry = self.lut[(1, 0, 8802, 1, 1, 2, 1, 4, 2, 1, entry)]
+            ret = mib_entry(sub_id=(1, 13))
+            self.assertIsNotNone(ret)
+            print(ret)
+        # Verfiy no mgmt address exit
+        for entry in range(3, 6):
+            mib_entry = self.lut[(1, 0, 8802, 1, 1, 2, 1, 4, 2, 1, entry)]
+            ret = mib_entry(sub_id=(1, 17))
+            self.assertIsNone(ret)
             print(ret)
 
     def test_local_port_identification(self):
