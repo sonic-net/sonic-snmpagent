c83b6d823b8bf191be59440caa9b9ea8591e5dc2
Qi Luo <qiluo-msft@users.noreply.github.com>
2023-02-04 00:56:21 +0000

    Fix: all zero route may have empty nexthop

--- src/sonic_ax_impl/mibs/ietf/rfc1213.py
+++ src/sonic_ax_impl/mibs/ietf/rfc1213.py
@@ -153,7 +153,7 @@ class NextHopUpdater(MIBUpdater):
                 ipn = ipaddress.ip_network(ipnstr)
                 ent = Namespace.dbs_get_all(self.db_conn, mibs.APPL_DB, routestr, blocking=False)
                 if ent:
-                    nexthops = ent["nexthop"]
+                    nexthops = ent.get("nexthop", "")
                     for nh in nexthops.split(','):
                         # TODO: if ipn contains IP range, create more sub_id here
                         sub_id = ip2byte_tuple(ipn.network_address)
