164a88d352bc0d58d0a6430451183a4814864bea
liuh-80 <liuh@microsoft.com>
2023-02-14 05:37:03 +0000

    Add UT

---
+++ tests/test_rfc1213.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from unittest import TestCase
+
+if sys.version_info.major == 3:
+    from unittest import mock
+else:
+    import mock
+
+modules_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(modules_path, 'src'))
+
+from sonic_ax_impl.mibs.ietf.rfc1213 import NextHopUpdater
+
+class TestNextHopUpdater(TestCase):
+
+    @mock.patch('sonic_ax_impl.mibs.Namespace.dbs_keys', mock.MagicMock(return_value=(["ROUTE_TABLE:0.0.0.0/0"])))
+    @mock.patch('sonic_ax_impl.mibs.Namespace.dbs_get_all', mock.MagicMock(return_value=({"nexthop": "10.0.0.1,10.0.0.3"})))
+    def test_NextHopUpdater_route_has_next_hop(self):
+        updater = NextHopUpdater()
+
+        with mock.patch('mibs.logger.warning') as mocked_warning:
+            updater.update_data()
+            
+            # check warning
+            mocked_warning.assert_not_called()
+
+        self.assertTrue(updater.route_list.count == 2)
+
+    @mock.patch('sonic_ax_impl.mibs.Namespace.dbs_keys', mock.MagicMock(return_value=(["ROUTE_TABLE:0.0.0.0/0"])))
+    @mock.patch('sonic_ax_impl.mibs.Namespace.dbs_get_all', mock.MagicMock(return_value=({})))
+    def test_NextHopUpdater_route_no_next_hop(self):
+        updater = NextHopUpdater()
+
+        with mock.patch('mibs.logger.warning') as mocked_warning:
+            updater.update_data()
+            
+            # check warning
+            expected = [
+                mock.call("Route has no nexthop: ROUTE_TABLE:0.0.0.0/0 \{\}")
+            ]
+            mocked_warning.assert_has_calls(expected)
+
+        self.assertTrue(updater.route_list == 0)
