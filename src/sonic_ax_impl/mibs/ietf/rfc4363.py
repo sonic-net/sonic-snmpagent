ed53a5ced371aefd64636c066877a8c4659a9919
liuh-80 <liuh@microsoft.com>
2023-07-21 06:23:27 +0000

    Fix MIBUpdater crash when SAI_FDB_ENTRY_ATTR_BRIDGE_PORT_ID attribute missing

--- src/sonic_ax_impl/mibs/ietf/rfc4363.py
+++ src/sonic_ax_impl/mibs/ietf/rfc4363.py
@@ -85,8 +85,13 @@ class FdbUpdater(MIBUpdater):
             if not ent:
                 continue
 
-            # Example output: oid:0x3a000000000608
-            bridge_port_id = ent["SAI_FDB_ENTRY_ATTR_BRIDGE_PORT_ID"][6:]
+            try:
+                # Example output: oid:0x3a000000000608
+                bridge_port_id = ent["SAI_FDB_ENTRY_ATTR_BRIDGE_PORT_ID"][6:]
+            except:
+                mibs.logger.error("SyncD 'ASIC_DB' includes invalid FDB_ENTRY '{}': failed to get bridge_port_id.".format(fdb_str))
+                continue
+
             if bridge_port_id not in self.if_bpid_map:
                 continue
             port_id = self.if_bpid_map[bridge_port_id]
