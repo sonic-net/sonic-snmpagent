c764d5c208107d6166b12363b29cd10746cc782a
Suvarna Meenakshi <sumeenak@microsoft.com>
2025-03-03 22:25:05 +0000

    Remove unwanted line
    Signed-off-by: Suvarna Meenakshi <sumeenak@microsoft.com>

--- src/sonic_ax_impl/mibs/vendor/cisco/ciscoPfcExtMIB.py
+++ src/sonic_ax_impl/mibs/vendor/cisco/ciscoPfcExtMIB.py
@@ -122,7 +122,6 @@ class PfcUpdater(MIBUpdater):
             counter_value = 0
             for lag_member in self.lag_name_if_name_map[self.oid_lag_name_map[oid]]:
                 member_counter = self._get_counter(mibs.get_index_from_str(lag_member), counter_name)
-                member_counter = self._get_counter(mibs.get_index_from_str(lag_member), counter_name)
                 if member_counter is not None:
                     counter_value += member_counter
                 else:
@@ -149,7 +148,6 @@ class PfcUpdater(MIBUpdater):
         if oid in self.oid_lag_name_map:
             counter_value = 0
             for lag_member in self.lag_name_if_name_map[self.oid_lag_name_map[oid]]:
-                member_counter = self._get_counter(mibs.get_index_from_str(lag_member), counter_name)
                 member_counter = self._get_counter(mibs.get_index_from_str(lag_member), counter_name)
                 if member_counter is not None:
                     counter_value += member_counter
